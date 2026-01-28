5156fa4a2083675742214aa8f876ff05b4582db1
Anuj <anujpawar1010@gmail.com>
2026-01-28 23:25:20 +0530

    COMMIT : ADDED PRISMA

---
+++ WEEK 10 (POSTGRESS - PRISMA)/Postgress/src/utils.ts
@@ -0,0 +1,10 @@
+import { Client } from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export async function getClient() {
+    const client = new Client(process.env.DATABASE_URL);
+    await client.connect();
+    return client;
+}
